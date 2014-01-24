a027fe4776361510c9881b571d8da0777d4bdbe6
Angela Jiang <angelajiang2014@u.northwestern.edu>
2014-01-24 14:40:01 -0600

    ADDED: bucket.go

---
+++ skeleton/src/kademlia/bucket.go
@@ -0,0 +1,53 @@
+package kademlia
+//Bucket definition and modules
+
+import (
+    "errors"
+)
+
+type Bucket struct {
+    Contacts [NUMBUCKETS]Contact
+    First int
+    Last int        //index to last contact in the bucket
+    Size int
+}
+
+func Update(contact Contact, bucket Bucket) error {
+    in_bucket := InBucket(contact, bucket)
+    is_full := IsFull(bucket)
+    switch {
+    case in_bucket:
+        //move to end of bucket
+        //remove first
+    case !in_bucket && !is_full:
+        bucket.Contacts[bucket.Last+1] = contact
+        bucket.Last++
+        bucket.Size++
+    case !in_bucket && is_full:
+        //ping head of bucket
+        //if head fails to respond
+            //drop head
+            //append contact to end of list
+        //else
+            //Move head to tail
+
+    }
+    return errors.New("function not implemented")
+}
+
+func InBucket(contact Contact, bucket Bucket) bool {
+    for  i:=0; i<bucket.Last; i++{
+        if bucket.Contacts[i].NodeID == contact.NodeID{
+            return true
+        }
+    }
+    return false
+}
+
+func IsFull(bucket Bucket) bool {
+    if NUMBUCKETS == bucket.Last{
+        return true
+    }
+    return false
+}
+
