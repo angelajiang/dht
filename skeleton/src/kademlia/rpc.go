31318e73a1ff6ef83f3ac5b24123cad8c0bb27c2
Ruba Merza <rmerzaa@gmail.com>
2014-02-06 12:10:06 -0800

    WROTE UP: AddNodesFromBucket IN-PROGRESS: PrefixDistance

--- skeleton/src/kademlia/rpc.go
+++ skeleton/src/kademlia/rpc.go
@@ -99,7 +99,7 @@ func FindClosestContacts(k *Kademlia, requestID ID) (closestContacts []Contact){
     distance := k.NodeID.Xor(requestID)
     //First 1 from MSB is index to closest bucket
     indices := DistanceToOnes(distance)
-    for index := range indices{
+    for index := range indices {
         //Add contacts from buckets[index] until closestContacts is full
         should_continue := AddNodesFromBucket(k, index, requestID, closestContacts)
         if !should_continue {
@@ -113,14 +113,53 @@ func FindClosestContacts(k *Kademlia, requestID ID) (closestContacts []Contact){
 
 func AddNodesFromBucket(k *Kademlia, index int, requestID ID, closestContacts []Contact)(IsFull bool){
     //Sort contacts in bucket
-    //Add contacts from bucket in sorted order
-    //Terminate when no more contacts in bucket or IsFull(closestContacts)
-    //Return false if added all contacts in bucket
-    //Return true if no more contacts in bucket
-    return true
+    IsFull = false
+    //make a sorted contacts slice
+    sorted_contacts := make([]Contact, 3)
+    //add an inital contact in there
+    sorted_contacts = append(sorted_contacts, k.Buckets[index].Contacts[0])
+    //loop through every contact in the bucket
+    for _, contact := range k.Buckets[index].Contacts {
+        //compare against distances of sorted contacts 
+        for n, sorted_contact := range sorted_contacts {
+           if PrefixDistance(contact.NodeID, k.NodeID) < PrefixDistance(sorted_contact.NodeID, k.NodeID) {
+               if n == 0 {
+                   //If it's closer than the first contact in sorted_contacts:
+                   //Prepend to list (must do this in a stupid way because
+                   //adding a contact to the front of a list is apparently not a
+                   //straightforward process!
+                   closestContacts = append(closestContacts, contact)
+                   closestContacts = append(closestContacts, sorted_contacts[(n+1):]...)
+                   //check if closestContacts is full
+                   if len(closestContacts) == 3 {
+                        IsFull = true
+                        return
+                   }
+               } else {
+                   //If it's closer than one of the other elements in
+                   //sorted_contacts:
+                   closestContacts = append(sorted_contacts[:(n-1)], contact, sorted_contacts[(n)])
+                   //check again for length of closestContact
+                   if len(closestContacts) == 3 {
+                        IsFull = true
+                        return
+                   }
+               }
+           }
+        }
+    }
+    //we check here in case we've looped through all the contacts and the loop
+    //terminated
+    if len(closestContacts) == 3 {
+        IsFull = true
+    }
+    return
 }
 
 
+func PrefixDistance(id ID, other ID) (dist int) {
+    return
+}
 func DistanceToOnes(distance ID)(ones []int){
     //Xor'ed result distance -> slice of bits in distance that are one from MSB->LSB 
     //ex) 1011 -> [1, 3, 4]
@@ -128,7 +167,7 @@ func DistanceToOnes(distance ID)(ones []int){
 
     return
 }
-    
+
 func ContactsToFoundNodes(contacts []Contact)(foundNodes []FoundNode){
     //Takes a splice of contacts and transforms it into a splice of foundNodes
     //Output can be stored in a FindNodeResult
