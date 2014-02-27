3715f93b3e581af1d38498e7c1cffbd042135d62
Angela Jiang <angelajiang2014@u.northwestern.edu>
2014-02-26 22:28:00 -0600

    FIXED: both tag inputs work now

--- skeleton/src/www/static/js/seedian.js
+++ skeleton/src/www/static/js/seedian.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 	//Manage tags
 	$(".tm-input").tagsManager();
-	var url = "localhost:9999"
-	var tags = $(".tm-input").tagsManager('tags');
+	//var url = "localhost:9999"
+	var tags = $("#tags").tagsManager('tags');
+	var tags2 = $("#tags2").tagsManager('tags');
+	/*
 	function processTags(tags) {
 	  $.post(url+"/processTags", "", 
 	  	function(data, status) {
@@ -10,4 +12,5 @@ $(document).ready(function(){
 		    $("#output").append(data);
   });
 }
+*/
 });
