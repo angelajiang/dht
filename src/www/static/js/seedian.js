0b3eb473b20d5c5c8d3a0a6dbdc910cf1c29d05c
Angela Jiang <angelajiang2014@u.northwestern.edu>
2014-03-03 04:45:53 -0600

    MODIFIED: post into a get call with extra parameters. datype=jsonp so we can get a response. TODO: response is not coming in in the correct format

--- src/www/static/js/seedian.js
+++ src/www/static/js/seedian.js
@@ -1,16 +1,27 @@
 $(document).ready(function(){
 	//Manage tags
 	$(".tm-input").tagsManager();
-	var url = "localhost:8080"
+	var url = "localhost:5555"
 	var tags = $("#tags").tagsManager('tags');
 	var tags2 = $("#tags2").tagsManager('tags');
 	processTags(tags)
 	function processTags(tags) {
-	  $.post("http://"+url+"/processTags", "mydata", 
-	  	function(data, status) {
-	  		console.log("hello");
-		    $("#output").append("<br>");
-		    $("#output").append(data);
-	  });
+		$.ajax({
+			url: "http://"+url+"/processTags",
+			data: { inputVal: "hi"},
+			success: function( data ) {
+					alert( "Data Loaded: " + data );
+			  		console.log("hello");
+				    $("#output").append("<br>");
+				    $("#output").append(data);
+				},
+			//headers: {"Access-Control-Allow-Origin": "http://localhost:8080"},
+			crossDomain: true,
+			error: function (xhr, ajaxOptions, thrownError) {
+			    console.log(xhr.status);
+	    		console.log(thrownError);
+			},
+			dataType: "jsonp"
+		});
 	}
 });
