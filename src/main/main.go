04af8d77a3bb897db6b90d5446b13532ad762f0c
Angela Jiang <angelajiang2014@u.northwestern.edu>
2014-03-27 18:24:29 -0400

    Fixed output format for iterativeFindNode command

--- src/main/main.go
+++ src/main/main.go
@@ -70,16 +70,9 @@ func main() {
         }
         // convert line from an array of bytes to a string array 
         cmdline := string(line)
-        cmdline_args := strings.Split(cmdline, " ")
-        command := cmdline_args[0]
+        cmdlineArgs := strings.Split(cmdline, " ")
+        command := cmdlineArgs[0]
         switch command {
-            case "setid":
-                //setid f
-                d := string(cmdline_args[1])
-                kadem.NodeID = kademlia.HexDigitToID(d, 20)
-            case "test":
-                //test
-                kademlia.TestBasicRPCs(kadem, firstPeerStr)
             case "ping":
                 //ping 123.12.12.0 1231
                 //ping 1111 --> will ping localhost:1111
@@ -90,7 +83,7 @@ func main() {
                 ping.Sender.Host = host
                 ping.Sender.Port = port
                 var pong_from_host kademlia.Pong
-                host_to_ping := cmdline_args[1]
+                host_to_ping := cmdlineArgs[1]
                 
                 if strings.Contains(host_to_ping, ":") {
                     remoteIP, remotePort := kademlia.PeerStrToHostPort(host_to_ping)
@@ -101,7 +94,7 @@ func main() {
                     }
                     log.Printf("pong MsgID: %v\n", pong_from_host.MsgID.AsString())
                 } else { 
-                    node_id, err := kademlia.FromString(cmdline_args[1])
+                    node_id, err := kademlia.FromString(cmdlineArgs[1])
                     if err != nil {
                         fmt.Printf("error converting from byte array to ID\n")
                     } else {
@@ -132,7 +125,7 @@ func main() {
                 fmt.Printf("%v\n", kadem.NodeID)
             case "local_find_value":
                 fmt.Printf("local_find_value")
-                key, err := kademlia.FromString(cmdline_args[1])
+                key, err := kademlia.FromString(cmdlineArgs[1])
                 if err != nil {
                     fmt.Printf("error converting from byte array to ID\n")
                 } else {
@@ -144,20 +137,20 @@ func main() {
                     }
                 }
             case "get_contact":
-                node_id, err := kademlia.FromString(cmdline_args[1])
+                node_id, err := kademlia.FromString(cmdlineArgs[1])
                 if err != nil {
                     fmt.Printf("error converting from byte array to ID\n")
                 } else {
                     kademlia.FindContactLocally(kadem, node_id)
                 }
             case "store":
-                if len(cmdline_args) != 4 {
-                    log.Printf("Error: Wrong number of arguments calling store. Expected 4, got %v\n", len(cmdline_args))
+                if len(cmdlineArgs) != 4 {
+                    log.Printf("Error: Wrong number of arguments calling store. Expected 4, got %v\n", len(cmdlineArgs))
                     break
                 }
-                node_id, err := kademlia.FromString(cmdline_args[1])
-                key, err := kademlia.FromString(cmdline_args[2])
-                val := []byte(cmdline_args[3])
+                node_id, err := kademlia.FromString(cmdlineArgs[1])
+                key, err := kademlia.FromString(cmdlineArgs[2])
+                val := []byte(cmdlineArgs[3])
                 contact, err := kademlia.FindContactLocally(kadem, node_id)
                 if err != nil {
                     //IterativeStore
@@ -174,7 +167,7 @@ func main() {
                 }
             case "find_node":
                 //find_node nodeID
-                node_id, err := kademlia.FromString(cmdline_args[1])
+                node_id, err := kademlia.FromString(cmdlineArgs[1])
                 if err != nil {
                     fmt.Printf("error converting from byte array to ID\n")
                 } else {
@@ -191,7 +184,7 @@ func main() {
                     }
                 }
             case "find_value":
-                key, err := kademlia.FromString(cmdline_args[2])
+                key, err := kademlia.FromString(cmdlineArgs[2])
                 if err != nil {
                     fmt.Printf("error converting from byte array to ID\n")
                 } else {
@@ -208,12 +201,12 @@ func main() {
                     }
                 }
             case "iterativeStore":
-                if len(cmdline_args) != 4 {
-                    log.Printf("Error: Wrong number of arguments calling iterativeStore. Expected 4, got %v\n", len(cmdline_args))
+                if len(cmdlineArgs) != 4 {
+                    log.Printf("Error: Wrong number of arguments calling iterativeStore. Expected 4, got %v\n", len(cmdlineArgs))
                     break
                 }
-                key, err := kademlia.FromString(cmdline_args[1])
-                val := []byte(cmdline_args[2])
+                key, err := kademlia.FromString(cmdlineArgs[1])
+                val := []byte(cmdlineArgs[2])
                 storedIn, err := kademlia.IterativeStore(kadem, key, val)
                 if err != nil{
                     log.Printf("%v\n", err)
@@ -221,36 +214,44 @@ func main() {
                 }
                 fmt.Printf("%v stored in %v\n", key, storedIn[len(storedIn)-1].NodeID)
             case "iterativeFindNode":
-                if len(cmdline_args) != 2 {
-                    log.Printf("Error: Wrong number of arguments calling iterativeFindNode. Expected 2, got %v\n", len(cmdline_args))
+                if len(cmdlineArgs) != 2 {
+                    fmt.Printf("Error in command \"iterativeFindNode\": command must be of the form \"iterativeFindNode nodeID\"\n")
                     break
                 }
-                node_id, err := kademlia.FromString(cmdline_args[1])
+                node_id, err := kademlia.FromString(cmdlineArgs[1])
                 closestContacts, err := kademlia.IterativeFindNode(kadem, node_id)
                 if err != nil {
                     log.Fatal("IterativeFindNode failed:\n", err)
                 }
-                fmt.Printf("closest contacts: %v\n", closestContacts)
-                for i, contact := range closestContacts {
-                    fmt.Printf("Contact %v ID: %v\n", i, contact.NodeID)
+                var closestIDs []kademlia.ID = make([]kademlia.ID, 0)
+                for _, contact := range closestContacts {
+                    closestIDs = append(closestIDs, contact.NodeID)
                 }
             case "iterativeFindValue":
                 //iterativeFindValue key(ID)
-                key, err := kademlia.FromString(cmdline_args[1])
+                key, err := kademlia.FromString(cmdlineArgs[1])
                 retID, foundValue, err := kademlia.IterativeFindValue(kadem, key)
                 if err != nil{
                     fmt.Printf("%v\n", err)
                     break
                 }
                 fmt.Printf("%v %v\n", retID, foundValue)
+            //Cases for debugging
+            case "setid":
+                //setid f
+                d := string(cmdlineArgs[1])
+                kadem.NodeID = kademlia.HexDigitToID(d, 20)
+            case "test":
+                //test
+                kademlia.TestBasicRPCs(kadem, firstPeerStr)
             case "is":
-                if len(cmdline_args) != 3 {
-                    log.Printf("Error: Wrong number of arguments calling ifn. Expected 3, got %v\n", len(cmdline_args))
+                if len(cmdlineArgs) != 3 {
+                    log.Printf("Error: Wrong number of arguments calling ifn. Expected 3, got %v\nex) is f message\n", len(cmdlineArgs))
                     break
                 }
-                d := string(cmdline_args[1])
+                d := string(cmdlineArgs[1])
                 key := kademlia.HexDigitToID(d, 20)
-                value := []byte(cmdline_args[2]) 
+                value := []byte(cmdlineArgs[2]) 
                 storedIn, err := kademlia.IterativeStore(kadem, key, value)
                 if err != nil{
                     log.Printf("%v\n", err)
@@ -260,11 +261,11 @@ func main() {
 
             case "ifn":
                 //ifn f
-                if len(cmdline_args) != 2 {
-                    log.Printf("Error: Wrong number of arguments calling ifn. Expected 2, got %v\n", len(cmdline_args))
+                if len(cmdlineArgs) != 2 {
+                    log.Printf("Error: Wrong number of arguments calling ifn. Expected 2, got %v\n", len(cmdlineArgs))
                     break
                 }
-                d := string(cmdline_args[1])
+                d := string(cmdlineArgs[1])
                 destID := kademlia.HexDigitToID(d, 20)
 
                 closestContacts, err := kademlia.IterativeFindNode(kadem, destID)
@@ -275,11 +276,11 @@ func main() {
 
             case "ifv":
                 //ifv f f
-                if len(cmdline_args) != 3 {
-                    log.Printf("Error: Wrong number of arguments calling ifn. Expected 3, got %v\n", len(cmdline_args))
+                if len(cmdlineArgs) != 3 {
+                    log.Printf("Error: Wrong number of arguments calling ifn. Expected 3, got %v\n", len(cmdlineArgs))
                     break
                 }
-                d := string(cmdline_args[1])
+                d := string(cmdlineArgs[1])
                 key := kademlia.HexDigitToID(d, 20)
                 retID, foundValue, err := kademlia.IterativeFindValue(kadem, key)
                 if err != nil{
